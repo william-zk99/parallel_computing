7d4693a96d5f08d24d37c34530a974ac2df2516a
fanweneddie <1164087609@qq.com>
2021-05-14 18:05:56 +0800

    for parallel with uniform division, aggregate by reduction

---
+++ lab1/par_pi_4.c
@@ -0,0 +1,36 @@
+/*
+ * method 4:
+ * calculate the approximate value of pi by 
+ * computing parallelism integral in range
+ * and aggregating the result by reduction
+ * 
+ * borrowed lab guide
+ * PB18111679 fanweneddie
+*/
+#include <stdio.h>
+#include <omp.h>
+static long num_steps = 100000;
+double step;
+#define NUM_THREADS 2
+
+void main () {
+    int i;
+    double pi = 0.0;
+    double sum = 0.0;
+    double x = 0.0;
+    step = 1.0/(double) num_steps;
+    // set 2 threads
+    omp_set_num_threads(NUM_THREADS);
+    // every thread can keep a copy of sum
+    // at last do "+" reduction to sum
+    #pragma omp parallel for reduction(+:sum) private(i,x) 
+    // thread 0 computes case 0 ~ 49999
+    // thread 1 computes case 50000 ~ 99999
+    for(i = 1;i <= num_steps; i++) {
+        x = (i - 0.5)*step;
+        sum += 4.0/(1.0 + x*x);
+    }
+    // sum is aggregated by reduction
+    pi = sum * step;
+    printf("%lf\n",pi);
+}
