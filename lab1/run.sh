e606a81d639fa1deda3e3febec139b1532a89a62
fanweneddie <1164087609@qq.com>
2021-05-15 12:22:08 +0800

    compile and run all

--- lab1/run.sh
+++ lab1/run.sh
@@ -1,2 +1,11 @@
+gcc -g -fopenmp par_pi_1.c -o par_pi_1
+gcc -g -fopenmp par_pi_2.c -o par_pi_2
+gcc -g -fopenmp par_pi_3.c -o par_pi_3
+gcc -g -fopenmp par_pi_4.c -o par_pi_4
+
+./par_pi_1
+./par_pi_2
+./par_pi_3
+./par_pi_4
 g++ -g -fopenmp PSRS.cpp -o PSRS
 ./PSRS 3 27 15 46 48 93 39 6 72 91 14 36 69 40 89 61 97 12 21 54 53 97 84 58 32 27 33 72 20
