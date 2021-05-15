ba3c99f85c29cb5ef6559a7558c2a76a54d5b487
fanweneddie <1164087609@qq.com>
2021-05-15 12:21:47 +0800

    add time counting

--- lab1/par_pi_1.c
+++ lab1/par_pi_1.c
@@ -7,38 +7,55 @@
  * PB18111679 fanweneddie
 */
 
-
 #include <stdio.h>
-// openmp library
 #include <omp.h>
+#include <time.h>
 
-// number of intervals in [0,1]
 static long num_steps = 100000;
-// the size of interval
 double step;
 #define NUM_THREADS 2
+#define TEST_TIME 10
 
 int main () {
     int i;
-    double x, pi, sum[NUM_THREADS];
+    double pi = 0.0;
+    double x = 0.0;
     step = 1.0/(double) num_steps;
-    // set 2 threads
-    omp_set_num_threads(NUM_THREADS);
-    // the start of parallelism domain for each thread
-    #pragma omp parallel private(i)
-    {
-        double x;
-        int id;
-        id = omp_get_thread_num();
-        // integral by getting sum of the interval
-        for (i = id, sum[id] = 0.0;i < num_steps; i = i + NUM_THREADS){
-            x = (i + 0.5)*step;
-            sum[id] += 4.0/(1.0 + x*x);
+    double sum[NUM_THREADS];
+    double result[TEST_TIME];
+    clock_t start,end;
+
+    start = clock();
+    for(int t = 0;t < TEST_TIME;++t){
+        x = 0;
+        // set 2 threads
+        omp_set_num_threads(NUM_THREADS);
+        // the start of parallelism domain for each thread
+        #pragma omp parallel private(i)
+        {
+            double x;
+            int id;
+            id = omp_get_thread_num();
+            // integral by getting sum of the interval
+            for (i = id, sum[id] = 0.0;i < num_steps; i = i + NUM_THREADS){
+                x = (i + 0.5)*step;
+                sum[id] += 4.0/(1.0 + x*x);
+            }
         }
+        // aggregate the results of each thread
+        for(i = 0, pi = 0.0;i < NUM_THREADS;++i) 
+            pi += sum[i] * step;
+        result[t] = pi;
     }
-    // aggregate the results of each thread
-    for(i = 0, pi = 0.0;i < NUM_THREADS;--i) 
-        pi += sum[i] * step;
-    
-    printf("%lf\n",pi);
+
+    end = clock();
+    pi = 0.0;
+    for(int t = 0; t < TEST_TIME;++t){
+        pi += result[t];
+    }
+    pi /= TEST_TIME;
+    printf("result 1: %lf\n",pi);
+    printf("time   1: %lfs\n",(double)(end-start)/CLOCKS_PER_SEC );
+    printf("-----------------------------------------\n");
+    return 0;
 }
